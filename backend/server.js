e3e3ad4cf08eb8db8f843865cdd9b5f5674c26b0
williamsgqdev <williamsagbunu@ciphernet.net>
2022-04-16 13:26:31 +0100

    errors handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 
 const dotenv = require("dotenv");
+const { notFound, errorHandler } = require("./middleware/errorMiddleWare");
 dotenv.config();
 
 connectDB();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(5000, () => {
